2d95c4f674c0a1def771fbca8f007524d021ae4c
Abdullah Ansari <abdullahansari@mac.lan>
2026-04-02 14:05:40 +0930

    added contact form logic and three second timer,

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 
 var express = require("express");
 var app = express();
-
+var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 var { engine } = require("express-handlebars");
 var path = require("path")
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.engine(".hbs", engine({
     extname: ".hbs",
-    defaultLayout: false
+    defaultLayout: false,
+    partialsDir: path.join(__dirname, "views/partials")
 }));
 app.set("view engine", ".hbs");
 
 app.get("/", function(req, res){
-    res.render("home.hbs");
+    res.render("home");
+});
+app.get("/contact", function(req, res){
+    res.render("contact");
+});
+
+app.post("/contact", function(req, res, next){
+    console.log("contact form posted");
+    console.log(req.body);
+res.render("contact", { message: "Thank you! Your message has been received."});
 });
 
 var port = process.env.PORT || 8080;
